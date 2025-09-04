64847a37fb848eeb751dc84f663e3a2b6d173538
sbiswas232 <139542832+sbiswas232@users.noreply.github.com>
2025-09-05 02:06:37 +0530

    Create variable.tf

---
+++ variable.tf
@@ -0,0 +1,14 @@
+variable "project" {
+  type    = string
+  default = "gha06sep25"
+}
+
+variable "region" {
+  type    = string
+  default = "ap-southeast-1"
+}
+
+variable "vpc_cidr" {
+  type    = string
+  default = "172.16.0.0/20"
+}
